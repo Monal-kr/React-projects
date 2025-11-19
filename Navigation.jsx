7c36fab481ecf2ad4f574fbecf6c19d8fa605e62
Monal-kr <monalkumar1996@gmail.com>
2025-11-19 10:59:25 +0530

    Add files via upload

---
+++ Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+    return (
+<nav className="container">
+            <div className="logo">
+                <img src="\Images\Logo.webp" alt="Logo" />
+            </div>
+            <ul>
+                <li>Menu</li>
+                <li>Location</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    );
+};
+
+export default Navigation;
